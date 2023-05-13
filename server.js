4164c15ff1deb9c8d798b034bd3ff3d4e6893eba
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-05-12 23:59:19 -0600

    Installed cors

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const env = require('dotenv').config();
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const swaggerAutogen = require('swagger-autogen')();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', require('./routes/contacts.js'));
