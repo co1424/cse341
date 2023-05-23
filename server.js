e6614e1a1fe2f4cb74e09e91510f62eac4be8aa2
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-05-23 09:00:14 -0600

    Lesson 05 - DONE&WORKING

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const swaggerDocument = require('./swagger.json');
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*'); //This * is to allow any site to access my API.
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
