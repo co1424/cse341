fec275c9f1bb42aa358785408d76c8f1a55e7fcb
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-05-25 21:51:44 -0600

    fixed swagger.json and contacts.routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = new express.Router();
 const contactsController = require('../controllers/contacts.js');
 const { validate } = require('../models/contactsModel.js');
-const validation = require('../validation/validator.js');
+const { contactValidationRules } = require('../validation/validator.js');
 
 // Creates a new contact
-router.post('/contact', validation.contactValidationRules,contactsController.createContact);
+router.post('/contact', contactValidationRules(),contactsController.createContact);
 // Gets all contacts
 router.get('/contacts', contactsController.getAllContacts);
 // Gets a contact by id
 router.get('/contact/:id', contactsController.getContactById);
 // Updates a contact by id
-router.put('/contact/:id', validation.contactValidationRules,contactsController.updateContactById);
+router.put('/contact/:id', contactValidationRules(),contactsController.updateContactById);
 // Deletes a contact by id
 router.delete('/contact/:id', contactsController.deleteContactById);
 
